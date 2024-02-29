f9f74f083a18879ebf2ead244bc61b658b656537
Shermin Ho <65299483+soonieboi@users.noreply.github.com>
2024-02-29 15:23:55 +0800

    Update settings.py for static files

--- irentstuff/settings.py
+++ irentstuff/settings.py
@@ -118,14 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/***/irentstuff/static'
-
+# STATIC_URL = '/static/'
+# STATIC_ROOT = '/home/***/irentstuff/static'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+    '/home/***/irentstuff/static/',
+] 
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
