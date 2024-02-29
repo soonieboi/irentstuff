8753373ffe2347e08e00f05c8d7fe1c52fccb109
Jonathan Ho <jonaelliho@hotmail.com>
2024-02-29 21:41:35 +0800

    FIrst test

--- irentstuff/settings.py
+++ irentstuff/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -118,12 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = '/home/***/irentstuff/static'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/***/irentstuff/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    '/home/***/irentstuff/static/',
+    '/home/***/irentstuff/static/'
 ] 
 
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
