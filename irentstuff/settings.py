21e30bb420d9dd5534bafd9f3356a10037dcca10
Shermin Ho <65299483+soonieboi@users.noreply.github.com>
2024-02-29 15:19:16 +0800

    Update settings.py

--- irentstuff/settings.py
+++ irentstuff/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ubuntu/irentstuff/static'
+STATIC_ROOT = '/home/***/irentstuff/static'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
