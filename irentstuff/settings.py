d50c647bbf667b211554139446a9145190f86608
Jonathan Ho <jonaelliho@hotmail.com>
2024-03-03 16:45:02 +0800

    commit

--- irentstuff/settings.py
+++ irentstuff/settings.py
@@ -118,14 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/***/irentstuff/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = '/home/***/irentstuff/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    '/home/***/irentstuff/static/'
-] 
+   BASE_DIR / "static",
+]
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = "/media/"
 
