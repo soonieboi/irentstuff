e8a8db9f745c753b41e49f5e7e6e1d1e9109e556
randallong <125627743+randallong@users.noreply.github.com>
2024-04-26 11:17:46 +0800

    Revert changes in settings.py

--- irentstuff/settings.py
+++ irentstuff/settings.py
@@ -145,15 +145,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'tmp/email-messages/'
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = 'tmp/email-messages/'
 
-# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.zoho.com'
-# EMAIL_HOST_USER = 'admin@irentstuff.app'
-# EMAIL_HOST_PASSWORD = 'R4P2k2jAKhSr'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
-# DEFAULT_FROM_EMAIL="admin@irentstuff.app"
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST_USER = 'admin@irentstuff.app'
+EMAIL_HOST_PASSWORD = 'R4P2k2jAKhSr'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL="admin@irentstuff.app"
 
